c13f901a0199553e89861da39430febc56287208
Adriano <adriano.selaviles@gmail.com>
2019-06-05 19:29:02 -0700

    separate multi apply into steps

--- src/main/java/com/adrianosela/pipeliner/entrypoint/Entrypoint.java
+++ src/main/java/com/adrianosela/pipeliner/entrypoint/Entrypoint.java
@@ -11,37 +11,45 @@ import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class Entrypoint {
 
   public static void main(String[] args) {
+    // create options
     PipelineOptions options = PipelineOptionsFactory.create();
 
+    // create pipeline
     Pipeline p = Pipeline.create(options);
 
-    // This example reads a public data set consisting of the complete works of Shakespeare.
-    p.apply(TextIO.read().from("data/sample"))
-        // This transform splits the lines in PCollection<String>, where each element is an
-        // individual word in Shakespeare's collected texts.
-        .apply(
+    // read file
+    PCollection<String> input = p.apply(TextIO.read().from("data/sample"));
+
+    // parse words
+    PCollection<String> words =
+        input.apply(
             FlatMapElements.into(TypeDescriptors.strings())
-                .via((String word) -> Arrays.asList(word.split("[^\\p{L}]+"))))
-        // We use a Filter transform to avoid empty word
-        .apply(Filter.by((String word) -> !word.isEmpty()))
-        // Concept #3: Apply the Count transform to our PCollection of individual words
-        .apply(Count.perElement())
-        // Apply a transform that formats our PCollection of word counts into a printable string
-        .apply(
+                .via((String word) -> Arrays.asList(word.split("[^\\p{L}]+"))));
+
+    // filter out empty words
+    PCollection<String> nonEmptyWords = words.apply(Filter.by((String word) -> !word.isEmpty()));
+
+    // count unique appearances of each word
+    PCollection<KV<String, Long>> wordCounts = nonEmptyWords.apply(Count.perElement());
+
+    // format onto the pretty string we want as output
+    PCollection<String> formattedOutputs =
+        wordCounts.apply(
             MapElements.into(TypeDescriptors.strings())
                 .via(
                     (KV<String, Long> wordCount) ->
-                        wordCount.getKey() + ": " + wordCount.getValue()))
-        // write transform to write the contents of a PCollection to a series of text files.
-        //
-        // By default, it will write to a set of files with names like wordcounts-00001-of-00005
-        .apply(new PrintOutput());
+                        wordCount.getKey() + ": " + wordCount.getValue()));
 
+    // print output
+    formattedOutputs.apply(new PrintOutput());
+
+    // block exit
     p.run().waitUntilFinish();
   }
 }
