2cc6f370ae1b17c8dd741c6830510cdae82ca6bd
Adriano <adriano.selaviles@gmail.com>
2019-06-09 00:40:15 -0700

    pipeline able to read from kinesis

--- src/main/java/com/adrianosela/pipeliner/pipelines/guardduty/GuardDuty.java
+++ src/main/java/com/adrianosela/pipeliner/pipelines/guardduty/GuardDuty.java
@@ -1,28 +1,47 @@
 package com.adrianosela.pipeliner.pipelines.guardduty;
 
+import com.adrianosela.pipeliner.ioutil.PrintToSTDOUT;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.kinesis.KinesisIO;
+import org.apache.beam.sdk.io.kinesis.KinesisRecord;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class GuardDuty {
 
   public static void main(String[] args) {
+
+    String STREAM_NAME = "stream-name";
+    String AWS_ACCESS_KEY_ID = "";
+    String AWS_SECRET_ACCESS_KEY = "";
+
     // create options
     PipelineOptions options = PipelineOptionsFactory.create();
 
     // create pipeline
     Pipeline p = Pipeline.create(options);
 
-    p.apply(
-        KinesisIO.read()
-            .withStreamName("streamName")
-            .withAWSClientsProvider("AWS_KEY", "AWS_SECRET", Regions.US_WEST_2)
-            .withInitialPositionInStream(InitialPositionInStream.TRIM_HORIZON));
+    // read from kinesis stream
+    PCollection<KinesisRecord> records =
+        p.apply(
+            KinesisIO.read()
+                .withStreamName(STREAM_NAME)
+                .withAWSClientsProvider(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, Regions.US_WEST_2)
+                .withInitialPositionInStream(InitialPositionInStream.TRIM_HORIZON));
 
-    // TODO
+    // convert data in stream to string
+    PCollection<String> recordData =
+        records.apply(
+            MapElements.into(TypeDescriptors.strings())
+                .via(record -> new String(record.getDataAsBytes())));
+
+    // print output to stdout
+    recordData.apply(new PrintToSTDOUT());
 
     // block exit
     p.run().waitUntilFinish();
